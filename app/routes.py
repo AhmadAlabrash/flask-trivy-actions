4da3e6b823ce3dc87e184e0c87bc6e370de732ef
AhmadAlabrash <86837193+AhmadAlabrash@users.noreply.github.com>
2026-04-01 20:42:19 +0200

    Edit Message

--- app/routes.py
+++ app/routes.py
@@ -2,4 +2,4 @@ from app import app
 
 @app.route("/")
 def home():
-    return "Hello from Flask CI/CD with GitHub Actions, Trivy, Docker Hub, and Argo CD!"
+    return "Hello from Flask CI/CD with GitHub Actions, Trivy, Docker Hub, and Argo CD!!!!"
